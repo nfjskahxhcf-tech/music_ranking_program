3a02105bc7026d3127c7273870831b5195f24be7
nfjskahxhcf-tech <nfjskahxhcf@gmail.com>
2026-02-15 04:44:29 +0000

    Reset tracks list and bump cache

--- runrank/static/service-worker.js
+++ runrank/static/service-worker.js
@@ -2,7 +2,7 @@
    - Cache the shell (index + manifest + icons)
    - Network-first for API (always try fresh)
 */
-const CACHE_NAME = "runrank-shell-v105";
+const CACHE_NAME = "runrank-shell-v106";
 
 const SHELL = [
   "/",                    // index.html (html=True라 /가 index 역할)
